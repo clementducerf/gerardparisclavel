7aa9364cbd23b3c363a0b34522c4c452dbe3b997
Clément Ducerf <9747465+clementducerf@users.noreply.github.com>
2018-03-27 14:25:13 +0200

    add of grapus template

---
+++ special-template-grapus.php
@@ -0,0 +1,48 @@
+<?php get_header();
+/*
+Template Name: Page Grapus
+*/
+global $post;
+$currentId = $post->ID;
+$parentId = wp_get_post_parent_id( $currentId );
+$parentUrl = get_page_link($parentId);
+?>
+
+<div id="header">
+    <div id="bouton-retour">
+        <?php
+        echo '<a href="'.$parentUrl.'"><img src="http://www.gerardparisclavel.fr/bnf/elements/main-noire.png"></a>';
+        ?>
+    </div>
+</div>
+
+
+<div id="content">
+    <section id="video">
+        <div class="videoContainer">
+            <div class="videoWrapper">
+                <iframe src="https://player.vimeo.com/video/258092918?badge=0" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+            </div>
+        </div>
+    </section>
+
+    <div class="description-grapus">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+            the_content();
+        endwhile; else: ?>
+            <p>Sorry, no posts matched your criteria.</p>
+        <?php endif; ?>
+    </div>
+</div>
+
+<?php
+if (has_children()){
+    $children = get_pages(array('child_of' => $currentId));
+    $childrenId = $children[0]->ID;
+    $childUrl =  get_permalink($childrenId);
+
+    echo '<a href="'.$childUrl.'" class="lien-galerie"></a>';
+}?>
+
+
+<?php get_footer(); ?>
